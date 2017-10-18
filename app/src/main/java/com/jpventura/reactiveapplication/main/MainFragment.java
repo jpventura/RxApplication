7b8aa237d7f8b754ade9e605c258d81e680765c4
JP Ventura <jp.ventura@smartmei.com.br>
2017-10-18 16:59:06 -0200

    :tada: Create application

---
+++ app/src/main/java/com/jpventura/reactiveapplication/main/MainFragment.java
@@ -0,0 +1,24 @@
+package com.jpventura.reactiveapplication.main;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jpventura.reactiveapplication.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainFragment extends Fragment {
+
+    public MainFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+}
